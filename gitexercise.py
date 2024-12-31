7755266f1ecd1685fc06d8011e86e75ee9b3745a
Lars <lazarus4jesus@hotmail.com>
2024-12-31 14:16:21 +0530

    Add changes to feature-branch

--- gitexercise.py
+++ gitexercise.py
@@ -3,6 +3,10 @@ from tkinter import *
 root = Tk()
 root.geometry("600x600+10+10")
 name = Label(text="hello there")
+
+name.pack()
+name = Label(text="first change")
+
 name.pack()
 print("this is code running")
 root.mainloop()
